df427cc6039c56a9ca4d1ffb4ee9d8bee7ba64a5
rafi0012 <rafiultalukdar007@gmail.com>
2025-11-09 12:15:03 +0600

    Setup Express and MongoDB connection

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+require('dotenv').config();
+
+
+// middle-ware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.DB_PASS}@cluster0.w0v9pwr.mongodb.net/?appName=Cluster0`;
+
+
+// MongoClient
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+    try{
+        await client.connect();
+
+        const db = client.db('tenth_assign_db');
+        const productsCollection = db.collection('products');
+
+        // Add database related api
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+            res.send(result);
+        });
+
+
+
+        
+        
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Server is running!')
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
